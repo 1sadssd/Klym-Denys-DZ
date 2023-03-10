42287c31782e06814672ed6b6de4418008a3b1c2
1sadssd <den20062345@gmail.com>
2023-03-11 00:32:41 +0200

    Добавьте файлы проекта.

---
+++ Klym Denys DZ/Klym Denys DZ.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include<Windows.h>
+
+using namespace std;
+
+int lastTwoDigits(int n) 
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    int result = 1;
+    for (int i = 0; i < n; i++) {
+        result = (result * 6) % 100;
+    }
+    return result;
+}
+
+
+int main() 
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    for (int i = 2; i <= 16; i++) {
+     cout << "6^" << i << " % 100 = " << lastTwoDigits(i) << endl;
+    }
+    return 0;
+}
+
